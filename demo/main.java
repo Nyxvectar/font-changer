0c16d6ad9ca2923691525c569567dbb9f39b33f0
Nyxvectar Yan <Nyxvectar@proton.me>
2021-08-21 18:18:20 +0800

    Initialization

--- demo/main.java
+++ demo/main.java
@@ -1,7 +1,11 @@
-// import java.util.HashMap;
-// import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Main {
+public class FontConverter {
+    private static final Map<Character, Character> charMap;
+    private static final Map<Character, Character> charMap2;
+    private static final Map<Character, Character> charMap3;
+
     public static void main(String[] args) {
         println("Hello")
     }
