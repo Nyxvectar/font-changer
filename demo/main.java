3980f77ef13818a349b36a0a854cd298fc2915eb
Nyxvectar Yan <Nyxvectar@proton.me>
2021-08-21 22:02:46 +0800

    Initialization

--- demo/main.java
+++ demo/main.java
@@ -105,6 +105,10 @@ public class FontConverter {
 
     private static String convertText(String text, Map<Character, Character> fontMap) {
         StringBuilder result = new StringBuilder();
-
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            result.append(fontMap.getOrDefault(c, c));
+        }
+        return result.toString();
     }
 }
